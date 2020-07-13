9da6ee34fbf08b9523182a130a2aa39f1b0d0506
Karthik SP <karthik@karthik>
2020-07-14 01:31:24 +0530

    attempt to connect to DB

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
+const mysql = require("mysql"); 
 const express = require("express");
 const request = require("request");
-const bodyParser = require("body-parser")    	
+const bodyParser = require("body-parser"); 	
+
 const app = express();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-var loginCredentions = {};
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+/*==========================
+ 	Mysql Connection
+==========================*/
+
+var connection = mysql.createConnection({
+	host : "localhost",
+	user : "newuser",
+	password : "K@rthik96",
+	database : "Movies_DB"
+	// port:"3000",
+	// socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+	// multipleStatements: true,
+});
+
+connection.connect(function(error){
+	if(error){
+		console.log(`error connecting to DB`);
+		return;
+	} else{
+		console.log(`Connection successfull`);
+	}
+});
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res){
-	res.render("home");
+	connection.query('SELECT * FROM users', function(error, results, fields){
+		if(!error){
+			console.log(error)
+		} else{
+			console.log(results[0]);
+			connection.end()
+			res.render("home");
+		}
+	});
 });
 
 app.get('/result', function(req, res){
@@ -30,7 +62,7 @@ app.get('/title/:movie_title', function(req, res){
 	request('http://www.omdbapi.com/?apikey=thewdb&t='+film, function(error, response, body){
 		if(!error && response.statusCode == 200){
 			let parseId = JSON.parse(body);
-			res.render('title', {parseId:parseId, loginCredentions:loginCredentions});
+			res.render('title', {parseId:parseId});
 		} else {
 			console.log(error);
 			console.log(response.statusCode);
@@ -47,9 +79,6 @@ app.get('/title/:movie_title/comments/new', function(req, res){
 
 app.post('/title/:movie_title/comments', urlencodedParser, function(req, res){
 	let movie = "req.params.movie_title";
-		loginCredentions.name = req.body.name;
-		loginCredentions.review = req.body.review;
-		console.log(loginCredentions)
 
 		res.redirect('/title/movie');
 });
