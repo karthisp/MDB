4f14592dd85aff3b8ecb295b52fba776c17803d4
Karthik SP <karthik@karthik>
2020-07-23 16:32:11 +0530

    sign up issue fixed git commit -m "sign up issue fixed"

--- app.js
+++ app.js
@@ -96,28 +96,22 @@ app.get('/sign_up', function(req, res){
 app.post('/sign_up', function(req, res){
 
 	connection.query('SELECT username FROM users WHERE username=?', req.body.username, function(error, result){
-		// if(result[0].username === req.body.username){
-		// 	console.log("Mama a person with this username already exists mama");
-		// 	res.redirect("/sign_up");
-		// 	return;
-		// } 
-		// else{
-		// 	bcrypt.hash(req.body.password, 10, function(err, hash){
-		// 		let user = {username:req.body.username, user_email:req.body.email, user_password:hash}
-		// 		connection.query("INSERT INTO users SET ?", user, function(sqlerror, result){
-		// 			if(sqlerror){
-		// 				console.log("sql signup error "+sqlerror);
-		// 				return;
-		// 			}
-		// 			res.redirect('/login');
-		// 		})
-		// 	})
-		// }
-
-		if(result === [] || result[0].username !== req.body.username){
-			console.log("I will consider "+ req.body.username)
-		} else{
-			console.log("Sorry boss cant let in");
+		if(result.length > 0 && result[0].username === req.body.username){
+			console.log("Mama a person with this username already exists mama");
+			res.redirect("/sign_up");
+			return;
+		} 
+		else{
+			bcrypt.hash(req.body.password, 10, function(err, hash){
+				let user = {username:req.body.username, user_email:req.body.email, user_password:hash}
+				connection.query("INSERT INTO users SET ?", user, function(sqlerror, result){
+					if(sqlerror){
+						console.log("sql signup error "+sqlerror);
+						return;
+					}
+					res.redirect('/login');
+				})
+			})
 		}
 	});
 });
